a35c2605200914bf7d0ba3f836ccb848f8e492ae
Joseph-plex <joseph@plexxis.com>
2014-04-24 11:38:03 -0400

    Change Log
    - Added Client Oracle Resource Extractor.

---
+++ Plex.MobileHub.Client.Oracle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Compression;
+using System.IO;
+namespace Plex.MobileHub.Client.Oracle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Todo Implement a way to specify a path to extract data too.
+            using (Stream stream = new MemoryStream(Oracle.Resources.Files))
+            using (ZipArchive archive = new ZipArchive(stream))
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                    entry.ExtractToFile(Environment.CurrentDirectory + @"\" + entry.Name);
+        }
+    }
+}
