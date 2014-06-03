7b8ab1864716a12b46692fb90355e65c5d69fc00
Joseph-plex <joseph@plexxis.com>
2014-06-03 15:42:40 -0400

    Implemented AddUserCompanyPermission.

--- Plex.MobileHub/Functionality/Clients/AddUserCompanyPermission.cs
+++ Plex.MobileHub/Functionality/Clients/AddUserCompanyPermission.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using Plex.MobileHub.Data.Tables;
+using Plex.MobileHub.Data;
+
 namespace Plex.MobileHub.Functionality.Clients
 {
-    //todo implement this
-    public class AddUserCompanyPermission
+    public class AddUserCompanyPermission : FunctionStrategyBase<Int32>
     {
+        public int Strategy(int appId, int dbCompanyUserId, int appUserTypeId = 0)
+        {
+            using(var connection = Utilities.GetConnection(true))
+                new CLIENT_DB_COMPANY_USER_APPS()
+                {
+                    APP_ID = appId,
+                    DB_COMPANY_USER_ID = dbCompanyUserId,
+                    APP_USER_TYPE_ID = ((appUserTypeId == 0)? (int?)null : appUserTypeId),
+                    DB_COMPANY_USER_APP_ID = Convert.ToInt32(connection.Query("select id_gen.nextval from dual")[0, 0])
+                }.Insert(connection);
+            return 0;
+        }
     }
 }
