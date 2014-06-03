37dd5bcbebc6c5a414c5a01dfa492cc61be70a13
Joseph-plex <joseph@plexxis.com>
2014-06-03 15:42:25 -0400

    Implemented ClientStatus in clients.

--- Plex.MobileHub/Functionality/Clients/ClientStatus.cs
+++ Plex.MobileHub/Functionality/Clients/ClientStatus.cs
@@ -2,11 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Plex.MobileHub.Data.Tables;
+using Plex.MobileHub.Data;
+using Plex.MobileHub.Repositories;
+using Plex.MobileHub.Objects.Clients;
 
-namespace MobileHub.Functionality.Clients
+
+namespace Plex.MobileHub.Functionality.Clients
 {
-    //todo implement this
-    public class ClientStatus
+    public class ClientStatus : FunctionStrategyBase<Int32>
     {
+        public int Strategy(int clientId)
+        {
+            if (!Connections.Instance.ConnectionExists(clientId))
+                return -1;
+            return Connections.Instance.Retrieve(clientId).IsConnected ? 0 : -1;
+        }
     }
 }
