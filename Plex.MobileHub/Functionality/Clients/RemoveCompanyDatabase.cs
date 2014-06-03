c7946b1682eabc6c0dca0c56673e696802abe75c
Joseph-plex <joseph@plexxis.com>
2014-06-03 15:42:12 -0400

    Implemented RemoveCompanyDatabase in Clients.

--- Plex.MobileHub/Functionality/Clients/RemoveCompanyDatabase.cs
+++ Plex.MobileHub/Functionality/Clients/RemoveCompanyDatabase.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using Plex.MobileHub.Data.Tables;
+using Plex.MobileHub.Data;
+
 namespace Plex.MobileHub.Functionality.Clients
 {
-    //todo implement this
-    public class RemoveCompanyDatabase
+    public class RemoveCompanyDatabase : FunctionStrategyBase<Int32>
     {
+        public int Strategy(int clientDbCompanyId)
+        {
+            CLIENT_DB_COMPANY_USERS.GetAll().First(p => p.USER_ID == clientDbCompanyId).Delete();
+            return 0;
+        }
+
     }
 }
