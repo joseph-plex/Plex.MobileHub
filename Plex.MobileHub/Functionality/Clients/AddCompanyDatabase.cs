515bfae0fbbc8f3ffde2a03833363b9fd3fc6581
Joseph-plex <joseph@plexxis.com>
2014-06-03 08:57:10 -0400

    Implemented AddCompanyDatabase for Clients.

---
+++ Plex.MobileHub/Functionality/Clients/AddCompanyDatabase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Plex.MobileHub.Data.Tables;
+using Plex.MobileHub.Data;
+
+
+namespace Plex.MobileHub.Functionality.Clients
+{
+    public class AddCompanyDatabase : FunctionStrategyBase<int>
+    {
+        public int Strategy(int clientId, string companyCode, string  connectionString)
+        {
+            using(var connection = Utilities.GetConnection(true))
+                new CLIENT_DB_COMPANIES()
+                {
+                    CLIENT_ID = clientId,
+                    COMPANY_CODE = companyCode,
+                    DATABASE_SID = connectionString,
+                    DB_COMPANY_ID = Convert.ToInt32(connection.Query("select id_gen.nextval from dual")[0, 0])
+                }.Insert();
+            return 0;
+        }
+    }
+}
