9bb25ebd3eb034d81cb220f6f7b03b32382302e8
Joseph-plex <joseph@plexxis.com>
2014-06-03 15:41:04 -0400

    Implemented RemoveUserCompanyPermission

--- Plex.MobileHub/Functionality/Clients/RemoveUserCompanyPermission.cs
+++ Plex.MobileHub/Functionality/Clients/RemoveUserCompanyPermission.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using Plex.MobileHub.Data.Tables;
+using Plex.MobileHub.Data;
+
 namespace Plex.MobileHub.Functionality.Clients
 {
-    //todo implement this
-    public class RemoveUserCompanyPermission
+    public class RemoveUserCompanyPermission : FunctionStrategyBase<Int32>
     {
+        public int Strategy(int clientDbCompanyUserAppId)
+        {
+            CLIENT_DB_COMPANY_USER_APPS.GetAll().First(p => p.DB_COMPANY_USER_APP_ID == clientDbCompanyUserAppId).Delete();
+            return 0;
+        }
     }
 }
