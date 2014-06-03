16b2ba0e7bfe0867349fa30a6ef3791994f5a658
Joseph-plex <joseph@plexxis.com>
2014-06-03 13:53:09 -0400

    Added stubs for functions that will be needed in the future.

---
+++ Plex.MobileHub/Functionality/Clients/AddClientUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Plex.MobileHub.Data.Tables;
+using Plex.MobileHub.Data;
+
+namespace Plex.MobileHub.Functionality.Clients
+{
+    public class AddClientUser : FunctionStrategyBase<Int32>
+    {
+        int Strategy(int clientId, string name, string password)
+        {
+            CLIENT_USERS user = new CLIENT_USERS();
+            using (var connection = Utilities.GetConnection(true))
+                user.USER_ID = Convert.ToInt32(connection.Query("select id_gen.nextval from dual")[0, 0]);
+            user.NAME = name;
+            user.PASSWORD = password;
+            user.CLIENT_ID = clientId;
+            user.Insert();
+            return user.USER_ID;
+        }
+    }
+}
---
+++ Plex.MobileHub/Functionality/Clients/RemoveCompanyDatabase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plex.MobileHub.Functionality.Clients
+{
+    public class RemoveCompanyDatabase
+    {
+    }
+}
