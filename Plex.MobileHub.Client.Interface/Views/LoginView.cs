307506b0668462ff4d016798d8ba4165a4e97827
Joseph-plex <joseph@plexxis.com>
2014-06-07 05:10:20 -0400

    Fixed a bug where controls not exist but where references in the code base.

--- Plex.MobileHub.Client.Interface/Views/LoginView.cs
+++ Plex.MobileHub.Client.Interface/Views/LoginView.cs
@@ -20,8 +20,6 @@ namespace Plex.MobileHub.Client.Interface.Views
         void LoginView_Load(object sender, EventArgs e)
         {
             Manager mgr = Manager.Instance;
-            textBox1.Text = mgr.ClientKey;
-            textBox2.Text = mgr.ClientId.ToString();
             textBox3.Text = mgr.IPAddress;
             textBox4.Text = mgr.Port.ToString();
             checkBox1.Checked = mgr.AutoLogin;
@@ -32,15 +30,11 @@ namespace Plex.MobileHub.Client.Interface.Views
             try {
                 Manager mgr = Manager.Instance;
 
-                var clientKey = textBox1.Text;
                 var port = Int32.Parse(textBox4.Text);
-                var clientId = Int32.Parse(textBox2.Text);
                 var address = IPAddress.Parse(textBox3.Text);
 
                 mgr.IPAddress = address.ToString();
                 mgr.AutoLogin = checkBox1.Checked;
-                mgr.ClientKey = clientKey;
-                mgr.ClientId = clientId;
                 mgr.Port = port;
             }
             catch (Exception x){
