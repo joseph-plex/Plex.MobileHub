8235ccba974fef1a856e7cdd8942c7e157ee1414
Joseph-plex <joseph@plexxis.com>
2014-06-07 02:01:36 -0400

    Added functionality for management.

--- Plex.MobileHub/management.asmx.cs
+++ Plex.MobileHub/management.asmx.cs
@@ -50,15 +50,6 @@ namespace Plex.MobileHub
                 return sr.ReadToEnd().Trim().Split(':')[1].Substring(1).Split('<')[0];
         }
      
-        public class ClientDpInfo
-        {
-            public String Name;
-            public Int32 ClientId;
-
-            public Boolean Online;
-            public DateTime Connected;
-            public DateTime OnlineDuration;
-        }
         [WebMethod]
         public List<Task> GetTaskData()
         {
@@ -136,6 +127,129 @@ namespace Plex.MobileHub
                 var user_constraints = Conn.Query("select c.* from user_tables t,user_constraints c where c.TABLE_NAME = t.TABLE_NAME and c.CONSTRAINT_TYPE = 'R'");
             }
             return Tree;
+        }
+
+        [WebMethod]
+        public int AppAdd(string authKey, string title, string description, bool isClientCustomApp)
+        {
+            APPS apps = new APPS(){ AUTH_KEY = authKey, TITLE = title, DESCRIPTION = description, IS_CLIENT_CUSTOM_APP = Convert.ToInt32(isClientCustomApp)};
+            using (var conn = Utilities.GetConnection(true)) { 
+                apps.APP_ID = Convert.ToInt32(conn.Query("select id_gen.nextval from dual")[0, 0]);
+                apps.Insert(conn);
+            }
+            return apps.APP_ID;
+        }
+
+        [WebMethod]
+        public int AppTableAdd(int appId, string name, string desc, int insert, int update, int delete, int select)
+        {
+            APP_TABLES aTables = new APP_TABLES() 
+            { 
+                APP_ID = appId, 
+                NAME = name, 
+                DESCRIPTION = desc, 
+                INSERT_ALLOWED = insert, 
+                UPDATE_ALLOWED = update, 
+                DELETE_ALLOWED = delete,
+                QUERY_ALLOWED = select 
+            };
+            using (var conn = Utilities.GetConnection(true))
+            {
+                aTables.TABLE_ID = Convert.ToInt32(conn.Query("select id_gen.nextval from dual")[0, 0]);
+                aTables.Insert(conn);
+            }
+            return aTables.TABLE_ID;
+        }
+
+        [WebMethod]
+        public int AppTableColumnAdd(int tabId, string nom, int seq, string type, int length, int prec, int scale, int nullable, int readOnly, int isLong, int isKey, string keyType, int unique, string desc)
+        {
+            using (var conn = Utilities.GetConnection(true)) 
+            { 
+                APP_TABLE_COLUMNS column = new APP_TABLE_COLUMNS()
+                {
+                    TABLE_COLUMN_ID =  Convert.ToInt32(conn.Query("select id_gen.nextval from dual")[0, 0]),
+                    TABLE_ID = tabId,
+                    COLUMN_NAME = nom,
+                    COLUMN_SEQUENCE = seq,
+                    DATA_TYPE = type,
+                    DATA_LENGTH = length,
+                    DATA_PRECISION = prec,
+                    DATA_SCALE = scale,
+                    ALLOW_DB_NULL = nullable,
+                    IS_READ_ONLY = readOnly, 
+                    IS_LONG = isLong,
+                    IS_KEY = isKey,
+                    KEY_TYPE = keyType,
+                    IS_UNIQUE = unique,
+                    DESCRIPTION = desc
+                };
+                column.Insert(conn);
+                return column.TABLE_COLUMN_ID;
+            }
+        }
+
+        [WebMethod]
+        public int ClientAdd(int clientId, string desc, string key)
+        {
+            new CLIENTS(){ CLIENT_ID = clientId, DESCRIPTION = desc, CLIENT_KEY = key }.Insert();
+            return clientId;
+        }
+
+        [WebMethod]
+        public int ClientAppsAdd(int appId, int clientId)
+        {
+            using(var conn = Utilities.GetConnection(true))
+            {
+                var cApps = new CLIENT_APPS() 
+                {
+                    CLIENT_APP_ID = Convert.ToInt32(conn.Query("select id_gen.nextval from dual")[0, 0]),
+                    APP_ID = appId,
+                    CLIENT_ID = clientId
+                };
+                cApps.Insert(conn);
+                return cApps.CLIENT_APP_ID;
+            }
+        }
+
+        [WebMethod]
+        public void AppRemove(int id)
+        {
+            var app = APPS.GetAll().FirstOrDefault(p => p.APP_ID == id);
+            if (app == null) throw new Exception("Invalid Application Id");
+            app.Delete();
+        }
+
+        [WebMethod]
+        public void AppTableRemove(int id)
+        {
+            var appTable = APP_TABLES.GetAll().FirstOrDefault(p => p.TABLE_ID == id);
+            if (appTable == null) throw new Exception("Invalid Application Table Id");
+            appTable.Delete();
+        }
+
+        [WebMethod]
+        public void AppTableColumnRemove(int id)
+        {
+            var appTableColumn = APP_TABLE_COLUMNS.GetAll().FirstOrDefault(p => p.TABLE_COLUMN_ID == id);
+            if (appTableColumn == null) throw new Exception("Invalid Application Table Column Id");
+            appTableColumn.Delete();
+        }
+
+        [WebMethod]
+        public void Clients(int id)
+        {
+            var client = CLIENTS.GetAll().FirstOrDefault(p => p.CLIENT_ID == id);
+            if (client == null) throw new Exception("Client does not exist");
+            client.Delete();
+        }
+
+        [WebMethod]
+        public void ClientAppRemove(int id) 
+        {
+            var clientApp = CLIENT_APPS.GetAll().FirstOrDefault(p => p.CLIENT_APP_ID == id);
+            if (clientApp == null) throw new Exception("Client App permission does not exist");
+            clientApp.Delete();
         }
     }
 }
