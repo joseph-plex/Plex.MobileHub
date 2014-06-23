4f6cfe450e6dffee274bffbc04cda1d897feeadd
Joseph-plex <joseph@plexxis.com>
2014-06-23 11:29:23 -0400

    Added Service Libraries
    Created updated version of Pmh.

---
+++ Plex.MobileHub.ServiceLibraries/APIServiceLibrary/APIService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Plex.MobileHub.ServiceLibraries.APIServiceLibrary
+{
+    [ServiceContract]
+    public interface APIService
+    {
+        void ConnectionConnect();
+        void ConnectionRelease();
+        void ConnectionStatus();
+        void DeviceRequestId();
+        void DeviceSynchronization();
+        void IUD();
+        void QryExecute();
+        void QueryDatabase();
+    }
+}
