8935c112a0c0a146dd00fd723f54f46934bc915d
Joseph-plex <joseph@plexxis.com>
2014-07-04 10:40:18 -0400

    Added New Set of Tests

---
+++ Plex.MobileHub.Tests/ApiTestImplementation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Plex.MobileHub.ServiceLibraries.APIServiceLibrary;
+using Plex.MobileHub.ServiceLibraries;
+using Plex.MobileHub.Data;
+using Plex.MobileHub.Data.Types;
+
+namespace Plex.MobileHub.Tests
+{
+    public class ApiTestImplementation : IApiService
+    {
+        public MethodResult ConnectionConnect(int clientId, int appId, string database, string user, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MethodResult ConnectionRelease(int connectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MethodResult ConnectionStatus(int connectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RegisteredQueryResult QryExecute(int connectionId, string QueryName, DateTime? Time = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public QryResult QueryDatabase(int connectionId, string Query, params object[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MethodResult DeviceRequestId(int connectionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DeviceSynchronizeMethodResult DeviceSynchronize(int connectionId, int versionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MethodResult IUD(int connection, object IUDData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Plex.MobileHub.Tests/ClientInteraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Plex.MobileHub.ServiceLibraries.APIServiceLibrary;
+
+namespace Plex.MobileHub.Tests
+{
+  
+    [TestClass]
+    public class ClientInteraction
+    {
+        const string myPipeService = "net.pipe://localhost";
+        const string myPipeServiceName = "ApiPipe";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (ServiceHost host = new ServiceHost())
+            {
+
+            }
+        }
+    }
+}
