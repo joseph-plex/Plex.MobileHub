eb13cfb53e43f9d97251fea73445c61e4ffd792f
Joseph-plex <joseph@plexxis.com>
2014-07-08 12:02:31 -0400

    Added Service Contract

---
+++ Plex.MobileHub.Client/IClientCommandService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Plex.MobileHub.Client
+{
+    [ServiceContract]
+    interface IClientCommandService
+    {
+        [OperationContract]
+        void LogIn(int id, string key);
+
+        [OperationContract]
+        void LogOff();
+
+    }
+}
