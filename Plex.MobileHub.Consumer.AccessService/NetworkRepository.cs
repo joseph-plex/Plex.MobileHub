18a11fe37048830c9ed53df677bbd30dcb4448b0
Joseph-plex <joseph@plexxis.com>
2014-08-06 16:34:41 -0400

    Created new Service For Accessing Data.
    Created a NetworkRepository to Abstract Accessing PMH Data.

---
+++ Plex.MobileHub.Consumer.AccessService/NetworkRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plex.Data;
+using System.ServiceModel;
+using Plex.MobileHub.ServiceLibrary.AccessService;
+namespace Plex.MobileHub.Consumer
+{
+    public class NetworkRepository<T> : IRepository<T> where T : IRepositoryEntry, new()
+    {
+        public event EventHandler<RepositoryOperationEventArgs> InsertEvent;
+
+        public event EventHandler<RepositoryOperationEventArgs> UpdateEvent;
+
+        public event EventHandler<RepositoryOperationEventArgs> DeleteEvent;
+
+        protected ChannelFactory<IAccessService> Factory
+        {
+            get
+            {
+                return factory;
+            }
+        }
+
+        private ChannelFactory<IAccessService> factory;
+        #region Public Methods
+        public NetworkRepository()
+        {
+            //todo Put some stuff in here for default ctor
+        }
+
+        public NetworkRepository(string uri)
+        {
+            EndpointAddress address = new EndpointAddress(uri);
+            BasicHttpBinding binding = new BasicHttpBinding() { UseDefaultWebProxy = false };
+            factory = new ChannelFactory<IAccessService>(binding, address);
+            factory.Open();
+        }
+
+        public NetworkRepository(ChannelFactory<IAccessService> channelFactory)
+        {
+            factory = channelFactory;
+        }
+        #endregion
+        #region IRepository Implementation
+        public IList<string> PrimaryKeys
+        {
+        }
+
+        public void Insert(T Entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T Entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Predicate<T> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(Predicate<T> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Retrieve(Predicate<T> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count(Predicate<T> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> RetrieveAll()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Plex.MobileHub.Service/AccessService.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Plex.MobileHub.ServiceLibrary.AccessService;
+
+namespace Plex.MobileHub.Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class AccessService : IAccessService
+    {
+        public void GetPrimayKeys(string typeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(string typeName, object Entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(string typeName, object Entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(string typeName, object[] Entry)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object[] RetrieveAll(string TypeName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
